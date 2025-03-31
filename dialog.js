54223d4ce1285e509f0e5f5424b7ac70d2343632
Sulaksha8 <sulaksha8@icloud.com>
2025-03-31 20:17:33 +1100

    Updated Home Page

--- dialog.js
+++ dialog.js
@@ -1,12 +1,21 @@
 const dialog = document.getElementById('productNone');
 const closeDialog = document.getElementById('close');
+const warning = document.getElementById('warning-text');
 
 document.addEventListener('DOMContentLoaded', function() {
     dialog.showModal();
-    console.log(`The dialog ${dialog} has been opened.`)
+    console.log(`The dialog ${dialog} has been opened.`);
+});
+
+warning.addEventListener('animationstart', function(){
+    closeDialog.setAttribute('disabled', 'true');
+});
+
+warning.addEventListener('animationend', function(){
+    closeDialog.removeAttribute('disabled');
 });
 
 closeDialog.addEventListener('click', function() {
     dialog.close();
-    console.log(`The dialog ${dialog} has been closed.`)
+    console.log(`The dialog ${dialog} has been closed.`);
 });
